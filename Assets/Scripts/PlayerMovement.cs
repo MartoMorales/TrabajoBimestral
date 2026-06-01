ad5758d50cc04d2d8873e3089f821479a99862ee
MartoMorales <martin.morales.larrozag@gmail.com>
2026-06-01 10:39:16 -0300

    Clase 2 👍

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject[] pickables;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pickables = GameObject.FindGameObjectsWithTag("Pickable");
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+    }
+     void OnTriggerEnter(Collider chocado)
+    {
+        for(int i =0; i<pickables.Length;i++)
+        {
+            if(pickables[i] == chocado.gameObject)
+            {
+                pickables[i].transform.SetParent(player.transform);
+                Debug.Log("El objeto ha sido agarrado");
+            }
+    }    
+}
+}
